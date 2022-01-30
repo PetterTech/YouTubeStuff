13d1bb2bc1d2913b1b282062df5cc1a20911f850
Petter Arnesen <parnesen@hotmail.com>
2022-01-30 22:27:54 +0100

    1st

--- AzureVMCreationBasedOnBicep/New-AzureServer.ps1
+++ AzureVMCreationBasedOnBicep/New-AzureServer.ps1
@@ -42,22 +42,118 @@ function New-AzureServer
 	    #Verify that name is within standards and matches region
 
         #Make sure servername is in uppercase
-
+        Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Converting servername to uppercase"
+        try {
+            Write-Verbose "Name before conversion: $($Name)"
+            $Name = $Name.ToUpper()
+            Write-Verbose "Name after conversion: $($Name)"
+        }
+        catch {
+            Write-Verbose "Something failed during name conversion"
+            $Error[0] | Out-Host
+            Write-Error "Failed to convert VM name to uppercase"
+        }
+        Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Converting servername to uppercase" -Completed
+            
         #Get session variables
 
         #Get username
         $DeployingUser = [Environment]::UserName
 
         #Connect to Azure
+        Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Connecting to Azure"
+        try {
+            Write-Verbose "Trying to connect to Azure"
+            Connect-AzAccount
+            Write-Verbose "Attempt complete"
+        }
+        catch {
+            Write-Verbose "Something failed during connection to Azure"
+            $Error[0] | Out-Host
+            Write-Error "Failed to connect to Azure"
+        }
+        Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Connecting to Azure" -Completed
 
-        #Get correct vnet
+        #Get correct region as a variabe
+        Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Getting correct Azure region"
+        try {
+            $AllAzRegions = Get-AzLocation
+        }
+        catch {
+            Write-Verbose "Something failed during Azure region getting"
+            $Error[0] | Out-Host
+            Write-Error "Failed to get correct Azure region"                 
+        }
 
-        #Get correct OS
+        try {
+            switch ($Region) {
+                "America" {$AzRegion = $AllAzRegions | Where-Object {$_.Location -eq "eastus"}}
+                "Europe" {$AzRegion = $AllAzRegions | Where-Object {$_.Location -eq "westeurope"}}
+                "Asia" {$AzRegion = $AllAzRegions | Where-Object {$_.Location -eq "southeastasia"}}
+                Default {$vNet = "Borked"}
+            }
+            if ($vNet -eq "Borked") {
+                Write-Error "Failed to match region to vNet"
+            }            
+        }
+        catch {
+            Write-Verbose "Something failed during vNet getting"
+            $Error[0] | Out-Host
+            Write-Error "Failed to get correct vNet"            
+        }
+        Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Getting correct Azure region" -Completed
 
+
+        #Get correct vNet
+        Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Getting correct vNet"
+        try {
+            switch ($Region) {
+                "America" {$vNet = Get-AzVirtualNetwork -Name vnet-america}
+                "Europe" {$vNet = Get-AzVirtualNetwork -Name vnet-europe}
+                "Asia" {$vNet = Get-AzVirtualNetwork -Name vnet-asia}
+                Default {$vNet = "Borked"}
+            }
+            if ($vNet -eq "Borked") {
+                Write-Error "Failed to match region to vNet"
+            }            
+        }
+        catch {
+            Write-Verbose "Something failed during vNet getting"
+            $Error[0] | Out-Host
+            Write-Error "Failed to get correct vNet"            
+        }
+        Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Getting correct vNet" -Completed
+
+        #Get correct VM image SKU
+        Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Getting correct VM image SKU"
+        try {
+            switch ($OS) {
+                "Windows" {$ImageSKU = Get-AzVMImage -Location $AzRegion.Location -PublisherName "microsoftWindowsServer" -Offer "windowsserver" -Skus "2022-datacenter-g2" | Select-Object -Last 1}
+                "Linux" {$ImageSKU = Get-AzVMImage -Location $AzRegion.Location -PublisherName "Canonical" -Offer "0001-com-ubuntu-server-focal" -Skus "20_04-lts-gen2" | Select-Object -Last 1}
+                Default {$ImageSKU = "Borked"}
+            }
+            if ($ImageSKU -eq "Borked") {
+                Write-Error "Failed to match OS to image"
+            }            
+        }
+        catch {
+            Write-Verbose "Something failed during image getting"
+            $Error[0] | Out-Host
+            Write-Error "Failed to get correct image"            
+        }
+
+        Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Getting correct VM image SKU" -Completed
+        
+        
+        Get-AzVMImageSku -Location $AzRegion.Location -PublisherName "microsoftWindowsServer" -Offer "windowsserver" | Where-Object {$_.Skus -eq "2022-datacenter"}
     }
 
     Process {
+        #Create bicep file based on template
+        
         #Deploy vm
+
+        #Tag vm
     }
 
     End {
