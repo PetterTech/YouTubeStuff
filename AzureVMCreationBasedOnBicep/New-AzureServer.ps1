a40be1fc11132bb8537a73dda64c3d69c7a1b409
Petter Arnesen <parnesen@hotmail.com>
2022-02-06 21:35:44 +0100

    first working version

--- AzureVMCreationBasedOnBicep/New-AzureServer.ps1
+++ AzureVMCreationBasedOnBicep/New-AzureServer.ps1
@@ -25,8 +25,7 @@ function New-AzureServer
         [CmdletBinding()] 
             Param (
                 [Parameter(Mandatory=$True,Position=0,HelpMessage='Name for the VM')]
-                [ValidatePattern('^az-.*',
-                     ErrorMessage = "{0} is not a servername in Azure. See Get-Help for more info")]
+                [ValidatePattern('^az-.*')]
                 [string]$Name,
                 [Parameter(Mandatory=$True,Position=1,HelpMessage='Region where the VM should be deployed')]
                 [ValidateSet('Europe','America','Asia')]
@@ -40,6 +39,34 @@ function New-AzureServer
                 )
     Begin {
 	    #Verify that name is within standards and matches region
+        Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Validating that VM name is within standards"
+        if ($Region -eq "Europe") {
+            Write-Verbose "Region is Europe"
+            if (!($Name -like "az-weu-*")) {
+                Write-Verbose "VM name does not follow standards, input was: $($Name)"
+                Write-Error "VM name does not comply with naming standard"
+            }
+            Write-Verbose "VM name complies with standards"
+        }
+
+        if ($Region -eq "America") {
+            Write-Verbose "Region is America"
+            if (!($Name -like "az-eus-*")) {
+                Write-Verbose "VM name does not follow standards, input was: $($Name)"
+                Write-Error "VM name does not comply with naming standard"
+            }
+            Write-Verbose "VM name complies with standards"
+        }
+
+        if ($Region -eq "Asia") {
+            Write-Verbose "Region is Asia"
+            if (!($Name -like "az-sea-*")) {
+                Write-Verbose "VM name does not follow standards, input was: $($Name)"
+                Write-Error "VM name does not comply with naming standard"
+            }
+            Write-Verbose "VM name complies with standards"
+        }
+        Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Validating that VM name is within standards" -Completed
 
         #Make sure servername is in uppercase
         Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Converting servername to uppercase"
@@ -54,7 +81,25 @@ function New-AzureServer
             Write-Error "Failed to convert VM name to uppercase"
         }
         Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Converting servername to uppercase" -Completed
-            
+
+        #Convert size to Azure readable format
+        Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Converting size to Azure readable size"
+        Write-Verbose "Converting size to Azure formatted size name. Input: $($Size)"
+        switch ($Size) {
+            "B2s" {$VMSize =  "Standard_B2s" }
+            "D2s" {$VMSize =  "Standard_D2s_v4" }
+            "D4s" {$VMSize =  "Standard_D4s_v4" }
+            "E4s" {$VMSize =  "Standard_E4s_v4" }
+            "F2s" {$VMSize =  "Standard_F2s_v2" }
+            Default {$VMSize = "Borked"}
+        }
+        Write-Verbose "Done converting size to Azure formatted size name. Output: $($VMsize)"
+
+        if ($VMSize -eq "Borked") {
+            Write-Error "Failed to match size to Azure readable size"
+        }
+        Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Converting size to Azure readable size" -Completed
+
         #Get session variables
 
         #Get username
@@ -64,7 +109,7 @@ function New-AzureServer
         Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Connecting to Azure"
         try {
             Write-Verbose "Trying to connect to Azure"
-            Connect-AzAccount
+            Connect-AzAccount | Out-Null
             Write-Verbose "Attempt complete"
         }
         catch {
@@ -73,6 +118,8 @@ function New-AzureServer
             Write-Error "Failed to connect to Azure"
         }
         Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Connecting to Azure" -Completed
+
+        #Get correct resource group based on region
 
         #Get correct region as a variabe
         Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Getting correct Azure region"
@@ -103,7 +150,6 @@ function New-AzureServer
         }
         Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Getting correct Azure region" -Completed
 
-
         #Get correct vNet
         Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Getting correct vNet"
         try {
@@ -123,6 +169,14 @@ function New-AzureServer
             Write-Error "Failed to get correct vNet"            
         }
         Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Getting correct vNet" -Completed
+
+        #Get subnet ID
+        try {
+            $subnet = $vNet | Get-AzVirtualNetworkSubnetConfig
+        }
+        catch {
+            Write-Error "Failed to get subnet"
+        }
 
         #Get correct VM image SKU
         Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Getting correct VM image SKU"
@@ -144,19 +198,50 @@ function New-AzureServer
 
         Write-Progress -Activity "Deploying Azure server" -Status "In preparation phase" -CurrentOperation "Getting correct VM image SKU" -Completed
         
-        
-        Get-AzVMImageSku -Location $AzRegion.Location -PublisherName "microsoftWindowsServer" -Offer "windowsserver" | Where-Object {$_.Skus -eq "2022-datacenter"}
     }
 
     Process {
-        #Create bicep file based on template
-        
-        #Deploy vm
+        #Create bicep parameter file based on template
+        #Grab default parameter file
+        try {
+            $DefaultParameterFile = Get-Content .\main.parameters.json | ConvertFrom-Json
+        }
+        catch {
+            Write-Error "Failed to import default parameter file"
+        }
 
-        #Tag vm
+        #Editing parameter file
+        $DefaultParameterFile.parameters.vmName.value = $Name
+        $DefaultParameterFile.parameters.location.value = $AzRegion.DisplayName
+        $DefaultParameterFile.parameters.deployingUser.value = $DeployingUser
+        $DefaultParameterFile.parameters.vmSize.value = $VMSize
+        $DefaultParameterFile.parameters.imageOffer.value = $ImageSKU.offer
+        $DefaultParameterFile.parameters.imageSku.value = $ImageSKU.skus
+        $DefaultParameterFile.parameters.imageVersion.value = $ImageSKU.Version
+        $DefaultParameterFile.parameters.imagePublisher.value = $ImageSKU.PublisherName
+        $DefaultParameterFile.parameters.subnetID.value = $subnet.Id
+
+        #Exporting to new parameter file
+        $DefaultParameterFile | ConvertTo-Json | Out-File .\newparameterfile.json
+
+        #Deploy vm
+        try {
+          New-AzResourceGroupDeployment -ResourceGroupName "rg-EULandingZone" -TemplateFile .\main.bicep -TemplateParameterFile .\newparameterfile.json
+        }
+        catch {
+          $Error[0] | Out-Host
+          Write-Error "Deployment failed"  
+        }
     }
 
     End {
         #Disconnect from Azure
+        try {
+        Disconnect-AzAccount | Out-Null
+            
+        }
+        catch {
+            
+        }
     }
 }
