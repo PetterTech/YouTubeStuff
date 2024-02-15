d6dff892d5b551a854d124786c843794e0c848d9
Petter Arnesen <parnesen@hotmail.com>
2024-02-15 15:27:16 +0100

    updated powershell version

--- AzureImageBuilderScripts/Install-PowerShell.ps1
+++ AzureImageBuilderScripts/Install-PowerShell.ps1
@@ -1,6 +1,6 @@
 #Download powershell
 $Filename = "powershell7.msi"
-Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.3.3/PowerShell-7.3.3-win-x64.msi" -OutFile $Filename
+Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/PowerShell/PowerShell/releases/download/v7.4.1/PowerShell-7.4.1-win-x64.msi" -OutFile $Filename
 
 #Install powershell silently
 Start-Process msiexec.exe -Wait -ArgumentList "/package $($Filename) /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=1 REGISTER_MANIFEST=1 USE_MU=1 ENABLE_MU=1 ADD_PATH=1"
